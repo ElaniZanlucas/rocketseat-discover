c530ceb24d781858afa4bc54e14c4bd3ba5aa239
ElaniZanlucas <emzan.06@gmail.com>
2022-07-18 19:05:26 -0400

    Timers

--- fundamentar/17 - nodeJS/clearInterval.js
+++ fundamentar/17 - nodeJS/clearInterval.js
@@ -3,4 +3,6 @@ const timeOut = 1000
 const checking = () => console.log('checking!')
 
 let interval = setInterval(checking, timeOut)
-clearInterval(interval)
+
+// Depois de um certo tempo, cancela o intervalo
+setTimeout( () => clearInterval(interval), 4000)
